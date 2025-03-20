ac63ac2b5959d1094483465aa7ee38e54b465b23
LuxScintilla <catherina.ea@gmail.com>
2025-03-20 14:54:47 +0000

    start front end modal and form

---
+++ client/src/components/Modal.js
@@ -0,0 +1,32 @@
+class Modal {
+  #modal;
+  #modalBtn;
+
+  constructor() {
+    this.#modal = document.querySelector("#modal");
+    this.#modalBtn = document.querySelector("#modal-btn");
+    this.addEventListeners();
+  }
+
+  addEventListeners() {
+    this.#modalBtn.addEventListener("click", this.open.bind(this));
+    window.addEventListener("click", this.outsideClick.bind(this));
+    document.addEventListener("closemodal", this.close.bind(this));
+  }
+
+  open() {
+    this.#modal.style.display = "block";
+  }
+
+  close() {
+    this.#modal.style.display = "none";
+  }
+
+  outsideClick(event) {
+    if (event.target === this.#modal) {
+      this.close();
+    }
+  }
+}
+
+export default Modal;
